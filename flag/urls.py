5bbc5a4ce7cdde4d0471f4c563a234bca5bb2b56
Fantomas42 <fantomas42@gmail.com>
2014-03-28 11:27:42 +0100

    Compatibility for Django >= 1.5

--- flag/urls.py
+++ flag/urls.py
@@ -6,7 +6,7 @@ To use the "flag_confirm" url, you can use the "flag_confirm_url" filter
 in your template, or "get_confirm_url_for_object" helper in flag.views
 """
 
-from django.conf.urls.defaults import *
+from django.conf.urls import *
 
 
 urlpatterns = patterns("",
